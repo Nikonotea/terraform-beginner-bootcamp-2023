1d32e1da283dde543336ac82d770cda26e78d979
mnechai <MNechai@wsgc.com>
2023-10-10 22:13:21 +0200

    #21 Add config drift documentation

--- providers.tf
+++ providers.tf
@@ -8,19 +8,11 @@ terraform {
   }
 
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "5.18.1"
     }
   }
-}
-
-provider "random" {
-  # Configuration options
 }
 
 provider "aws" {
